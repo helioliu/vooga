90135cc41119374daf53db496f12aec1e9cea0de
Chris Graybeal <git@github.com:kaitlynduke/Tivoo.git>
2012-04-01 20:33:03 -0400

    adding state files

---
+++ src/States/UnderWaterState.java
@@ -0,0 +1,43 @@
+package States;
+
+import com.golden.gamedev.object.Sprite;
+
+public class UnderWaterState implements State{
+
+	@Override
+	public void UpKey(Sprite s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DownKey(Sprite s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void RightKey(Sprite s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void LeftKey(Sprite s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SpaceKey(Sprite s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void collide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
