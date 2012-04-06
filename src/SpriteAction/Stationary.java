cc34fbb7ba2d4b755de2b9237dccf93d2baa0af3
Ben Mozenter <Mozenterb@gmail.com>
2012-04-06 02:13:42 -0400

    Add Enemy Actions

---
+++ src/SpriteAction/Stationary.java
@@ -0,0 +1,20 @@
+package SpriteAction;
+
+import com.golden.gamedev.object.Sprite;
+
+import core.EventManager;
+
+public class Stationary extends SpriteAction {
+
+    public Stationary(Sprite s) {
+        super(s);
+        EventManager em = EventManager.getEventManager();
+        em.registerEventListener("stay stationary", this);
+    }
+
+    public void actionPerformed(String event)
+    {
+        mySprite.setMovement(0, 0); 
+    }
+
+}
---
+++ src/SpriteAction/WalkLeft.java
@@ -0,0 +1,20 @@
+package SpriteAction;
+
+import com.golden.gamedev.object.Sprite;
+
+import core.EventManager;
+
+
+public class WalkLeft extends SpriteAction{
+
+    public WalkLeft(Sprite s) {
+        super(s);
+        EventManager em = EventManager.getEventManager();
+        em.registerEventListener("walking left", this);
+    }
+    
+    public void actionPerformed(String event){
+        mySprite.setMovement(.025, 270);
+    }
+
+}
