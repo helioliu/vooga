f5923d29f064425073ceca7283c36c6d2b7a6c55
Michael Hoyle <mah68@duke.edu>
2012-04-06 02:20:25 -0400

    finished cutscenes for part 1, with some testing code

--- src/cutscenes/Cutscene.java
+++ src/cutscenes/Cutscene.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import core.EventManager;
+import core.EventListener;
 
-public class Cutscene {
+public class Cutscene implements EventListener {
 	List<SpriteAutomation> myAutomations;
+	private boolean cutsceneOn;
 	
 	public Cutscene() {
 		myAutomations = new ArrayList<SpriteAutomation>();
-		init();
+		cutsceneOn = false;
+		EventManager.getEventManager().registerEventListener("cutscene-begin", this);
 	}
 	
 	public void addSpriteAutomation(SpriteAutomation automation) {
@@ -21,14 +24,22 @@ public class Cutscene {
 		myAutomations.addAll(automation);
 	}
 	
-	public void init() {
-		EventManager.getEventManager().sendEvent("cutscene-start");
-	}
-	
 	public void update(long timeElapsed) {
-		for(SpriteAutomation automation : myAutomations) {
-			automation.update(timeElapsed);
+		if(cutsceneOn) {
+			cutsceneOn = false;
+			for(SpriteAutomation automation : myAutomations) {
+				automation.update(timeElapsed);
+				// If any of the automations are still working, don't quit.
+				cutsceneOn = cutsceneOn | automation.isCurrentlyAnimating();
+			}
 		}
 		
+	}
+
+	@Override
+	public void actionPerformed(String eventName) {
+		if (eventName.equals("cutscene-begin")) {
+			cutsceneOn = true;
+		}
 	}
 }
--- src/cutscenes/SpriteAutomation.java
+++ src/cutscenes/SpriteAutomation.java
@@ -1,10 +1,12 @@
 package cutscenes;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
+import java.util.Scanner;
+import java.util.regex.*;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.Timer;
 
@@ -15,35 +17,63 @@ public class SpriteAutomation {
 	List<AutomatedAction> actionList;
 	Timer currentTimer;
 	Integer currentPosition;
+	boolean currentlyAnimating;
 
-	public SpriteAutomation(Sprite s, String filePath) {
+	public SpriteAutomation(Sprite s, String filePath) throws FileNotFoundException, BadFileFormatException {
 		File f = new File(filePath);
 		sprite = s;
 		actionList = parseAutomation(f);
 		currentPosition = 0;
-		currentTimer = new Timer(actionList.get(0).getDuration());
+		currentTimer = new Timer(0);
+		currentlyAnimating = true;
 
 	}
 
-	public List<AutomatedAction> parseAutomation(File f) {
+	public List<AutomatedAction> parseAutomation(File f) throws FileNotFoundException, BadFileFormatException {
 		List<AutomatedAction> automation = new ArrayList<AutomatedAction>();
-		// TODO do this
+		Scanner s = new Scanner(f);
+
+		while (s.hasNext()) {
+			String l = s.nextLine();
+			String[] line = l.split(":");
+			if (line.length != 2)
+				throw new BadFileFormatException("Cannot read line: "+l);
+			String eventName = line[0];
+			Integer duration = Integer.parseInt(line[1]);
+//			System.out.println(eventName+" "+Integer.toString(duration));
+			AutomatedAction current = new AutomatedAction(eventName, duration);
+			automation.add(current);
+		}
+
 		return automation;
 	}
 
 	public void update(long timeElapsed) {
-		if(currentTimer.action(timeElapsed)){
-			EventManager.getEventManager().sendEvent(actionList.get(currentPosition).getEventName());
-			currentPosition ++;
-			checkIfDone();
-			currentTimer = new Timer(actionList.get(currentPosition).getDuration());
-		}
 		
+		if (currentTimer.action(timeElapsed) & currentlyAnimating) {
+			EventManager.getEventManager().sendEvent(
+					actionList.get(currentPosition).getEventName());
+			currentTimer = new Timer(actionList.get(currentPosition)
+					.getDuration());
+			currentPosition++;
+			checkIfDone();
+			
+		}
+
+	}
+
+	private void checkIfDone() {
+		if (currentPosition >= actionList.size()) {
+			currentlyAnimating = false;
+		}
+	}
+
+	public boolean isCurrentlyAnimating() {
+		return currentlyAnimating;
 	}
 	
-	private void checkIfDone() {
-		if(currentPosition >= actionList.size()) {
-			// TODO do this
-		}
+	public List<AutomatedAction> getActionList() {
+		return actionList;
 	}
+
 }
