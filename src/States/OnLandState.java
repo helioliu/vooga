7804dfd45140028f220b87c7a7061fedf005b99c
Bryan Antigua <antigua_B@hotmail.com>
2012-04-11 14:55:25 -0400

    Adding to OnLandState

--- src/States/OnLandState.java
+++ src/States/OnLandState.java
@@ -1,5 +1,8 @@
 package States;
 
+import sprites.TestCharacterWithStates;
+import hudDisplay.DecrementBarAction;
+
 import com.golden.gamedev.object.Sprite;
 
 import SpriteAction.JumpAction;
@@ -17,5 +20,6 @@ public class OnLandState extends CharacterState{
 		myMap.put("right", new MoveRight(s));
 		myMap.put("left", new MoveLeft(s));
 		myMap.put("floor collide", new StandAction(s));
+		myMap.put("got hit", new DecrementBarAction((TestCharacterWithStates) s));
 	}
 }
