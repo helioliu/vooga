467c4ef262daf51644fabba3ed6e65bebc42401c
Andrew Bentley <andrew.ericson.bentley@gmail.com>
2012-04-13 04:54:20 -0400

    yup

--- src/core/test/Main.java
+++ src/core/test/Main.java
@@ -7,7 +7,6 @@ import com.golden.gamedev.GameLoader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
-	
 		GameLoader game = new GameLoader();
 		game.setup(new Chris_TestGame(), new Dimension(640, 480), false);
 		game.start();
