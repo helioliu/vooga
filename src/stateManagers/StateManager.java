8808881c3161f76b5b710a870dc76f8480e4f61f
Chris Graybeal <git@github.com:kaitlynduke/Tivoo.git>
2012-04-20 02:25:23 -0400

    now can have multiple active states

--- src/stateManagers/StateManager.java
+++ src/stateManagers/StateManager.java
@@ -2,6 +2,7 @@ package stateManagers;
 
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.List;
 
 import stateTransitions.StateTransition;
 
@@ -17,31 +18,49 @@ import core.EventListener;
 public class StateManager{
 	private Sprite mySprite;
 
-	private State currentState;
+	private List<State> currentStates;
 	
 	public StateManager(Sprite s, State startingState)
 	{
 		mySprite = s;
-		currentState = startingState;
-		currentState.activateAllListeners();
+		currentStates.add(startingState);
+		currentStates.get(0).activateAllListeners();
 	}
 	
 
-	
+	public void addState(State s)
+	{
+		currentStates.add(s);
+		s.activateAllListeners();
+	}
+	public void removeState(State s)
+	{
+		currentStates.remove(s);
+		s.deactivateAllListeners();
+	}
 	public void changeState(State s)
 	{
-		if(currentState!=s)
+		for(State cur : currentStates)
 		{
-			currentState.deactivateAllListeners();
-			currentState = s;
-			currentState.activateAllListeners();
+			cur.deactivateAllListeners();
 		}
+		currentStates.clear();
+		s.activateAllListeners();
+		currentStates.add(s);
+		
 		
 	}
 	
-	public boolean compareToCurrent(State s)
+	public boolean isCurrentlyActive(State s)
 	{
-		return currentState.equals(s);
+		for(State cur : currentStates)
+		{
+			if(cur.equals(s))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	
