c167a99dba64c8cbae11e5e6622f5825f25cb904
Bryan Antigua <antigua_B@hotmail.com>
2012-04-24 19:06:32 -0400

    forgot one...

--- src/hudDisplay/GraphicItem.java
+++ src/hudDisplay/GraphicItem.java
@@ -4,7 +4,6 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import sprites.GeneralSprite;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.Timer;
 
