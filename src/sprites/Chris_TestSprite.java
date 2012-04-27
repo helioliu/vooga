185a839517c7acfac4f73602d4d494dcec20e250
Chris Graybeal <git@github.com:kaitlynduke/Tivoo.git>
2012-04-26 22:18:49 -0400

    idk

--- src/sprites/Chris_TestSprite.java
+++ src/sprites/Chris_TestSprite.java
@@ -25,7 +25,7 @@ import States.OnLandState;
 import States.RegularMotionState;
 import States.ReverseMotionState;
 import States.State;
-import States.TeleJumpPowerup;
+import States.JetPackPowerup;
 import States.WalkingRightState;
 
 import com.golden.gamedev.object.Sprite;
@@ -38,11 +38,11 @@ public class Chris_TestSprite extends StateSprite{
 		super();
 
 		State s1 = new InAirState(this);
-		setGravity(0.000); //.002
+		setGravity(0.002); //.002
 		getStateManager().addState(s1);
 		StateTransition land = new ReplaceStateTransition(getStateManager(), "landed",  new OnLandState(this), s1);
 		StateTransition jump = new ReplaceStateTransition(getStateManager(), "jumped", s1, new OnLandState(this));
-		StateTransition powerup = new AddStateTransition(getStateManager(), "pwrup", new TeleJumpPowerup(this));
+		StateTransition powerup = new AddStateTransition(getStateManager(), "pwrup", new JetPackPowerup(this));
 		setMyStats(new HashMap<String, Stat>());	
 		land.activate();
 		jump.activate();
