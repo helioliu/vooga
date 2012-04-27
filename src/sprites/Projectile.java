330534d1290246bd774029a629dd5ed0d4fb32ae
Ben Mozenter <Mozenterb@gmail.com>
2012-04-26 20:53:27 -0400

    added homing functionality

--- src/sprites/Projectile.java
+++ src/sprites/Projectile.java
@@ -1,5 +1,6 @@
 package sprites;
 import java.awt.image.BufferedImage;
+import java.util.Vector;
 
 import levelEditor.*;
 import game.*;
@@ -8,38 +9,38 @@ import com.golden.gamedev.object.Sprite;
 
 
 public class Projectile extends Sprite {
-	// indicates whether this enemy has been show to screen or not
-	boolean show = false;
-	Platformer game;
+    // indicates whether this enemy has been show to screen or not
+    boolean show = false;
+    Platformer game;
 
 
-	
 
-	public Projectile(BufferedImage image) {
-		super(image,0,0);
-		
-	}
+    public Projectile(BufferedImage image) {
+        super(image,0,0);
 
-	public Projectile(Projectile projectile, Platformer currentGame) {
-		super(projectile.getImage(),0,0);
-		show=projectile.show;
-		game=currentGame;
-	}
-	
-	public void home(Sprite target){
-	double angleToTarget= getAngle(target.getHorizontalSpeed(), target.getVerticalSpeed());
-	double velocity = getVelocity(target.getHorizontalSpeed(), target.getVerticalSpeed());
-	this.setMovement(velocity, angleToTarget);
-	}
-	
-	public static double getAngle(double xVelocity, double yVelocity){
-	    return Math.toDegrees(Math.atan2(yVelocity, xVelocity));
-	}
-	
-	public static double getVelocity(double xVelocity, double yVelocity){
-	    return Math.sqrt(Math.pow(xVelocity, 2)+ Math.pow(yVelocity, 2));
-	}
-	
-	
-	
+    }
+
+    public Projectile(Projectile projectile, Platformer currentGame) {
+        super(projectile.getImage(),0,0);
+        show=projectile.show;
+        game=currentGame;
+    }
+
+    public void fireAtTarget(Sprite target){
+       double angleToTarget = Math.atan2( (target.getY()-this.getY()),(target.getX()-this.getX()));
+       
+       angleToTarget = Math.toDegrees(angleToTarget);
+       
+       if(angleToTarget< 0){
+           angleToTarget+=360;
+       }
+       angleToTarget+=90;
+       System.out.println(angleToTarget);
+        this.setMovement(.04, angleToTarget);
+    }
+
+  
+
+
+
 }
--- src/sprites/WalkingBadGuy.java
+++ src/sprites/WalkingBadGuy.java
@@ -1,24 +1,80 @@
 package sprites;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.Timer;
+
 import stateTransitions.ChangeStateTransition;
 import stateTransitions.StateTransition;
 import States.RegularMotionState;
+import States.StationaryState;
 import States.WalkingDownState;
 import States.WalkingLeftState;
 import States.WalkingRightState;
 import States.WalkingUpState;
 
 public class WalkingBadGuy extends Enemy {
+    
+    private ArrayList<HomingProjectile> projectiles = new ArrayList<HomingProjectile>();
+    
+    public ArrayList<HomingProjectile> getProjectiles() {
+        return projectiles;
+    }
 
     public WalkingBadGuy(){
         super();
+        enableShoot = false;
         StateTransition one = new ChangeStateTransition(getStateManager(), "walk right", new WalkingRightState(this));
         StateTransition two = new ChangeStateTransition(getStateManager(), "walk left", new WalkingLeftState(this));
         StateTransition three = new ChangeStateTransition(getStateManager(), "walk up", new WalkingUpState(this));
         StateTransition four = new ChangeStateTransition(getStateManager(), "walk down", new WalkingDownState(this));
+        StateTransition five = new ChangeStateTransition(getStateManager(), "stay stationary", new StationaryState(this));
         one.activate();
         two.activate();
         three.activate();
         four.activate();
+        five.activate();
     }
+
+    public void Shoot( long elapsedTime, Timer fireRate, SpriteGroup Projectile, BufferedImage Image, Sprite s){
+        Projectile shot;
+        if(enableShoot == true){
+            shot = new Projectile(Image);
+            shot.setLocation( this.getX()+15, this.getY()-5 );
+            shot.fireAtTarget(s);
+
+            Projectile.add(shot);
+            enableShoot = false;
+            fireRate.refresh();
+        }
+        else { 
+            if (fireRate.action( elapsedTime  ))  
+                enableShoot = true; 
+
+        }
+    }
+
+    public void Shoot2(long elapsedTime, Timer fireRate, SpriteGroup Projectile, BufferedImage Image, Sprite s){
+        HomingProjectile shot;
+        if(enableShoot == true){
+            shot = new HomingProjectile(Image);
+            shot.setLocation( this.getX()+15, this.getY()-5 );
+            shot.fireAtTarget(s, elapsedTime);
+            Projectile.add(shot);
+            enableShoot = false;
+            fireRate.refresh();
+            projectiles.add(shot);
+        }
+        else { 
+            if (fireRate.action( elapsedTime  ))  
+                enableShoot = true; 
+
+        }
+    }
+
 }
+
