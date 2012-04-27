79eca028c79dd2e1c04f0b4535061e3fb482d412
Chris Graybeal <git@github.com:kaitlynduke/Tivoo.git>
2012-04-27 02:08:29 -0400

    added ability to gain life

--- src/States/OnLandState.java
+++ src/States/OnLandState.java
@@ -1,9 +1,17 @@
 package States;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import sprites.GeneralSprite;
+import sprites.StateSprite;
+import stateTransitions.ChangeStateTransition;
 
 import com.golden.gamedev.object.Sprite;
 
+import SpriteAction.AddLife;
 import SpriteAction.ChangeImage;
 import SpriteAction.JumpAction;
 import SpriteAction.MoveLeft;
@@ -12,14 +20,15 @@ import SpriteAction.StandAction;
 
 public class OnLandState extends State{
 	
-	public OnLandState(GeneralSprite s)
+	public OnLandState(StateSprite s)
 	{
 		super(s);
 		addAction("Up", new JumpAction(s));
 		addAction("Right", new MoveRight(s));
 		addAction("Left", new MoveLeft(s));
 		addAction("floor collide", new StandAction(s));
-		addAction("mushroom", new ChangeImage(s));
+		addAction("mushroom", new AddLife(s));
+
 		//addAction("got hit", new DecrementBarAction((TestCharacterWithStates) s));
 		s.setGravity(0.002);
 	}
